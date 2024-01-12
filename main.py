e4d7c361b479cc01c406a7b19a162b5a30397183
lmraine <55120024+lmraine@users.noreply.github.com>
2024-01-12 09:38:17 -0600

    fix main

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #remove pass and update function
 def remove_digits(user_string):
+   pass
+
+
+if __name__ == '__main__':
+    #remove pass and your code goes here
+    pass
+
+#Solution
+'''def remove_digits(user_string):
     new = ""
     for c in user_string:
         if c.isdigit():
             continue
         else:
             new += c
-    return new
-
-
-if __name__ == '__main__':
-    #remove pass and your code goes here
-    pass
+    return new'''
